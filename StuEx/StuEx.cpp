f30a3b70a9efe32964c9f32b7cb1f75f44c7c72a
David Logic <58322362+DavidLogic@users.noreply.github.com>
2023-04-24 19:31:43 +0800

    First Commit,添加项目文件。

---
+++ StuEx/StuEx.cpp
@@ -0,0 +1,14 @@
+﻿// StuEx.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+// Copyright (c) Logic.Studio 
+// Members:刘明鑫、刘承锦、马浩然
+// SPDX-License-Identifier: MIT Lisense
+
+#include <iostream>
+#include "CFilePicker.h"
+int main()
+{
+    std::cout << "Hello World!\n";
+    CFilePicker();
+}
+
